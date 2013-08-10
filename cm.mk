cc01d1bd3cc6cd7e605582ff66ce90a32ba1108b
Scott Warner <Tortel1210@gmail.com>
2013-08-09 23:09:31 -0400

    Change fingerprint and device name to match the Play Edition
    Change-Id: I02ac13a507066232d108fd0b49a3bc48d293eaa4

--- cm.mk
+++ cm.mk
@@ -9,7 +9,11 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jfltexx TARGET_DEVICE=jfltexx BUILD_FINGERPRINT="samsung/jfltexx/jflte:4.2.2/JDQ39/I9505XXUAMDE:user/release-keys" PRIVATE_BUILD_DESC="jfltexx-user 4.2.2 JDQ39 I9505XXUAMDE release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=jgedlteue \
+    TARGET_DEVICE=jgedlte \
+    BUILD_FINGERPRINT="samsung/jgedlteue/jgedlte:4.3/JWR66V.S014/130801:user/release-keys" \
+    PRIVATE_BUILD_DESC="jgedlteue-user 4.3 JWR66V.S014 130801 release-keys"
 
 PRODUCT_DEVICE := jfltexx
 PRODUCT_NAME := cm_jfltexx
