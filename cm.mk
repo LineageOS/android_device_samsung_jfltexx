570e37f8552dc7a94af0be3079442dd26b5f3a5b
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-08-27 17:19:55 +0800

    jfltexx: Update build fingerprint
    OJ2 -> PF4
    
    Change-Id: I6b8d0110c812effc0b3dfccd8a43129b856b27e8

--- cm.mk
+++ cm.mk
@@ -9,9 +9,8 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 PRODUCT_BUILD_PROP_OVERRIDES += \
   PRODUCT_NAME=jfltexx \
   TARGET_DEVICE=jfltexx \
-  BUILD_FINGERPRINT=samsung/jfltexx/jflte:5.0.1/LRX22C/I9505XXUHOJ2:user/release-keys \
-  PRIVATE_BUILD_DESC="jfltexx-user 5.0.1 LRX22C I9505XXUHOJ2 release-keys"
+  BUILD_FINGERPRINT=samsung/jfltexx/jflte:5.0.1/LRX22C/I9505XXUHPF4:user/release-keys \
+  PRIVATE_BUILD_DESC="jfltexx-user 5.0.1 LRX22C I9505XXUHPF4 release-keys"
 
 PRODUCT_DEVICE := jfltexx
 PRODUCT_NAME := cm_jfltexx
-
