f857c30db3841cc78e65aaf44ab3b19ae7bfd5a7
ItsOnlyBinary <git@itsonlybinary.com>
2017-01-11 17:09:57 +0000

    jf: cm -> lineage
    Change-Id: I37e21c2183b0bc9a125f63dd1143fc3b55edd7bf

--- lineage.mk
+++ lineage.mk
@@ -1,6 +1,6 @@
 $(call inherit-product, device/samsung/jfltexx/full_jfltexx.mk)
 
-# Inherit some common CM stuff.
+# Inherit some common Lineage stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
@@ -10,5 +10,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
   PRIVATE_BUILD_DESC="jfltexx-user 5.0.1 LRX22C I9505XXUHPF4 release-keys"
 
 PRODUCT_DEVICE := jfltexx
-PRODUCT_NAME := cm_jfltexx
+PRODUCT_NAME := lineage_jfltexx
 
