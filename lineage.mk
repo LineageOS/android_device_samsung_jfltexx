45f1c631a3fd256b9286e884a996726fe9845330
ItsOnlyBinary <git@itsonlybinary.com>
2017-01-11 16:36:39 +0000

    jf: cm -> lineage
    Change-Id: Id6856cb57186b8cf81a0bc644a497593aa543026

--- lineage.mk
+++ lineage.mk
@@ -3,7 +3,7 @@ $(call inherit-product, device/samsung/jfltexx/full_jfltexx.mk)
 # Enhanced NFC
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
-# Inherit some common CM stuff.
+# Inherit some common Lineage stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
@@ -13,5 +13,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
   PRIVATE_BUILD_DESC="jfltexx-user 5.0.1 LRX22C I9505XXUHPF4 release-keys"
 
 PRODUCT_DEVICE := jfltexx
-PRODUCT_NAME := cm_jfltexx
+PRODUCT_NAME := lineage_jfltexx
 
