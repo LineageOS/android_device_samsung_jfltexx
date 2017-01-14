63471b380a66de96e8c9fb4528f52b174ca9185c
Zhao Wei Liew <zhaoweiliew@gmail.com>
2017-01-15 01:26:14 +0800

    jfltexx: Rebrand to LineageOS
    Change-Id: Iab856a46e0991d1ece40352c1c9fe654a63530b1

--- lineage.mk
+++ lineage.mk
@@ -10,5 +10,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
   PRIVATE_BUILD_DESC="jfltexx-user 5.0.1 LRX22C I9505XXUHPF4 release-keys"
 
 PRODUCT_DEVICE := jfltexx
-PRODUCT_NAME := cm_jfltexx
+PRODUCT_NAME := lineage_jfltexx
 
