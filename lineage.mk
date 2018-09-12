cb2d2d31bc362a7a444a31f25e9a3367f98d3451
ripee <ripee.dev@gmail.com>
2018-09-12 14:11:12 +0200

    Fix device naming

--- lineage.mk
+++ lineage.mk
@@ -1,14 +1,14 @@
 $(call inherit-product, device/samsung/jfltexx/full_jfltexx.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
   PRODUCT_NAME=jfltexx \
   TARGET_DEVICE=jfltexx \
-  BUILD_FINGERPRINT=samsung/jfltexx/jflte:5.0.1/LRX22C/I9505XXUHPF4:user/release-keys \
-  PRIVATE_BUILD_DESC="jfltexx-user 5.0.1 LRX22C I9505XXUHPF4 release-keys"
+  PRIVATE_BUILD_DESC="jfltexx-user 5.0.1 LRX22C I9505XXUHQK1 release-keys"
+
+BUILD_FINGERPRINT := samsung/jfltexx/jflte:5.0.1/LRX22C/I9505XXUHQK1:user/release-keys
 
 PRODUCT_DEVICE := jfltexx
 PRODUCT_NAME := lineage_jfltexx
-
