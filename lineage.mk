dbdad7107774b81549b61d61c072acb6f21d8253
ShevT <shev_t@mail.ru>
2018-09-19 16:09:26 +0300

    jfltexx: Update product build.prop overrides
    Change-Id: I5d334b78d0d7b3eb6ef4d91f9998810eb8963b4b

--- lineage.mk
+++ lineage.mk
@@ -3,12 +3,18 @@ $(call inherit-product, device/samsung/jfltexx/full_jfltexx.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += \
-  PRODUCT_NAME=jfltexx \
-  TARGET_DEVICE=jfltexx \
-  BUILD_FINGERPRINT=samsung/jfltexx/jflte:5.0.1/LRX22C/I9505XXUHPF4:user/release-keys \
-  PRIVATE_BUILD_DESC="jfltexx-user 5.0.1 LRX22C I9505XXUHPF4 release-keys"
-
-PRODUCT_DEVICE := jfltexx
+## Device identifier. This must come after all inclusions
 PRODUCT_NAME := lineage_jfltexx
+PRODUCT_DEVICE := jfltexx
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
 
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_MODEL=GT-I9505 \
+    PRODUCT_NAME=jfltexx \
+    PRODUCT_DEVICE=jflte \
+    TARGET_DEVICE=jflte \
+    PRIVATE_BUILD_DESC="jfltexx-user 5.0.1 LRX22C I9505XXUHPF4 release-keys"
+
+BUILD_FINGERPRINT := "samsung/jfltexx/jflte:5.0.1/LRX22C/I9505XXUHPF4:user/release-keys"
