dd10308e0dfd9c6ad4b0488a67edcdd74e8b717f
dkati <dkatikaridis@gmail.com>
2018-12-14 01:20:24 +0200

    jfltexx: Move vidc images to jf-common
    Change-Id: I531ac043537df31a5ceefd22a4c02b8c6a7f1b60

--- Android.mk
+++ Android.mk
@@ -28,17 +28,5 @@ include $(call all-subdir-makefiles,$(LOCAL_PATH))
 
 include $(CLEAR_VARS)
 
-# Create /firmware links
-VIDC_IMAGES := \
-    vidc.b00 vidc.b01 vidc.b02 vidc.b03 vidc.mdt
-
-VIDC_SYMLINKS := $(addprefix $(TARGET_OUT_ETC)/firmware/,$(notdir $(VIDC_IMAGES)))
-$(VIDC_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
-	@echo "Firmware link: $@"
-	@mkdir -p $(dir $@)
-	@rm -rf $@
-	$(hide) ln -sf /firmware/image/$(notdir $@) $@
-
-ALL_DEFAULT_INSTALLED_MODULES += $(VIDC_SYMLINKS)
 
 endif
