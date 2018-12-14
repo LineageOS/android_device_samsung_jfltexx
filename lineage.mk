f356dad4d64c6b4babd3cba29c7db676d1259885
dkati <dkatikaridis@gmail.com>
2018-12-14 22:55:22 +0200

    lineage: Remove CM inheritance
    Change-Id: I740f2c8d0764ca970ff2e51cfc0ac9b806cafcc9

--- lineage.mk
+++ lineage.mk
@@ -1,8 +1,5 @@
 $(call inherit-product, device/samsung/jfltexx/full_jfltexx.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
-
 PRODUCT_BUILD_PROP_OVERRIDES += \
   PRODUCT_NAME=jfltexx \
   TARGET_DEVICE=jfltexx \
