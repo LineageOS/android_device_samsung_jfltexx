a60c4b45229f747705fa4686f4f27500e67f46cb
Nolen Johnson <johnsonnolen@gmail.com>
2019-01-29 05:36:27 +0000

    jfltexx: Fully remove system.prop
    Change-Id: I1b8595c3030db161902d27dd02c2134706c97ad9

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -28,6 +28,3 @@ include device/samsung/jf-common/BoardConfigCommon.mk
 
 # Assert
 TARGET_OTA_ASSERT_DEVICE := GT-I9505,GT-I9505G,i9505,i9505g,jflte,jfltetmo,jfltexx,jgedlte
-
-# Properties
-TARGET_SYSTEM_PROP += device/samsung/jfltexx/system.prop
