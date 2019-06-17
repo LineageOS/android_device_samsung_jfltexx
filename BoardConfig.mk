f70f02db18db0d71dc5bca852e9bf19a99998627
Arne Coucheron <arco68@gmail.com>
2019-06-17 23:45:45 +0300

    jfltexx: Update copyrights
    Change-Id: I872924a921e05418de09dc2888069dacdf7d0a68
    Co-Authored-By: dkati <dkatikaridis@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,4 +1,5 @@
-# Copyright (C) 2009 The CyanogenMod Project
+# Copyright (C) 2013-2016 The CyanogenMod Project
+# Copyright (C) 2017-2019 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,14 +12,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-#
-# This file sets variables that control the way modules are built
-# thorughout the system. It should not be used to conditionally
-# disable makefiles (the proper mechanism to control what gets
-# included in a build is to use PRODUCT_PACKAGES in a product
-# definition file).
-#
 
 # inherit from common jf
 include device/samsung/jf-common/BoardConfigCommon.mk
