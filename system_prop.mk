e037cd2124550b955f3b6cf99731cbe2fa5c6c6e
Arne Coucheron <arco68@gmail.com>
2019-06-17 23:41:35 +0300

    jfltexx: Call device's system prop using makefile
    * Also declare the vendor security patch
    
    Change-Id: Ib59f8bae67c94dcfc4674049bc5f905ef40df3c4
    Co-Authored-By: dkati <dkatikaridis@gmail.com>

---
+++ system_prop.mk
@@ -0,0 +1,7 @@
+# Vendor security patch level
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.lineage.build.vendor_security_patch=2017-08-01
+
+# RIL
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.telephony.default_network=9
