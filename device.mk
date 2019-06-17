a426b9f08706a84b388639526ab76bd1ef414bce
Arne Coucheron <arco68@gmail.com>
2019-06-17 23:45:45 +0300

    jfltexx: Update copyrights and README
    Change-Id: I872924a921e05418de09dc2888069dacdf7d0a68
    Co-Authored-By: dkati <dkatikaridis@gmail.com>

--- device.mk
+++ device.mk
@@ -1,5 +1,5 @@
-#
-# Copyright (C) 2011 The CyanogenMod Project
+# Copyright (C) 2013-2016 The CyanogenMod Project
+# Copyright (C) 2017-2019 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,7 +12,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 
 ## (2) Also get non-open-source specific aspects if available
 $(call inherit-product-if-exists, vendor/samsung/jf-gsm-common/jf-gsm-common-vendor.mk)
