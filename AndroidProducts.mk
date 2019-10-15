f679720c007fb1ba4776a9a30b6a6138f069ac32
dkati <dkatikaridis@gmail.com>
2019-10-16 00:02:25 +0300

    jfltexx: Switch from add_lunch_combo to COMMON_LUNCH_CHOICES
    Change-Id: I09c96de84febd5ff547a8c4e28faeb366908c1fe

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,3 +14,7 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_jfltexx.mk
+
+COMMON_LUNCH_CHOICES := \
+	lineage_jfltexx-userdebug \
+	lineage_jfltexx-eng
--- vendorsetup.sh
+++
@@ -1,2 +0,0 @@
-add_lunch_combo lineage_jfltexx-eng
-add_lunch_combo lineage_jfltexx-userdebug
